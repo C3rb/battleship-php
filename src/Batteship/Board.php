c54152d3a2e5250046e365dcd364cf3c233b93b4
Wiktor Henc <whenc@internationalfx.com>
2021-12-10 13:30:37 +0100

    Feature 4 - fix

--- src/Batteship/Board.php
+++ src/Batteship/Board.php
@@ -7,15 +7,30 @@ class Board
     /** @var array */
     private $fields;
 
+    private $rows;
+    private $columns;
+
     public function __construct(int $rows, int $columns)
     {
+        $this->rows = $rows;
+        $this->columns = $columns;
         $this->fields = [];
         for ($r = 0; $r < $rows; $r++) {
             $this->fields[Letter::$letters[$r]] = [];
             for ($c = 1; $c <= $columns; $c++) {
-                $this->fields[Letter::$letters[$r]][] = null;
+                $this->fields[Letter::$letters[$r]][$c] = null;
             }
         }
+    }
+
+    public function getRows()
+    {
+        return $this->rows;
+    }
+
+    public function getColumns()
+    {
+        return $this->columns;
     }
 
     public function setField(Position $position, string $value)
