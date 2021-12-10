da877d72d4e6f3a2e38ccae53efd9cca34619ece
Wiktor Henc <whenc@internationalfx.com>
2021-12-10 14:09:17 +0100

    Lighter color

--- src/Batteship/Color.php
+++ src/Batteship/Color.php
@@ -7,7 +7,7 @@ class Color
     const DEFAULT_GREY = "\e[39m";
     const MAGENTA = "\e[95m";
     const CADET_BLUE = "\e[1;34m";
-    const RED = "\e[1;31m";
+    const RED = "\e[1m\e[1;31m";
     const YELLOW = "\e[0;33m";
     const ORANGE = "\e[0;33m";
     const CHARTREUSE = "\e[1;32m";
